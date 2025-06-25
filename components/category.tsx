aea517efa6c8c554ee7e33675fec226e6f046ce5
jude7733 <judeputhussery360@gmail.com>
2025-06-25 13:29:44 +0530

    fix layout

--- components/category.tsx
+++ components/category.tsx
@@ -8,49 +8,29 @@ export default async function Category() {
   return (
     <div className="flex flex-col w-full px-6 mx-auto">
       <h2 className="text-4xl">Categories</h2>
-      {
-        category.map((category: string) => (
-          <div key={category} className="mb-8 w-full grid grid-cols-2 grid-rows-2">
-            {category === "electronics" ? (
-              <BackgroundGradient className="mb-8">
-                <BentoGrid>
-                  <BentoCard
-                    name="Electronics"
-                    background={
-                      <img
-                        src="https://ik.imagekit.io/lrigu76hy/tailark/electronics.jpg?updatedAt=1745733473768"
-                        alt="Electronics"
-                        className="h-full w-full object-cover"
-                      />
-                    }
-                    description="Explore the latest in electronics, from smartphones to laptops."
-                    href="/electronics"
-                    cta="Shop Now"
-                  />
-                </BentoGrid>
-              </BackgroundGradient>
-            ) : (
-              <BackgroundGradient className="mb-8">
-                <BentoGrid>
-                  <BentoCard
-                    name={category.charAt(0).toUpperCase() + category.slice(1)}
-                    background={
-                      <img
-                        src={`https://ik.imagekit.io/lrigu76hy/tailark/${category}.jpg?updatedAt=1745733473768`}
-                        alt={category}
-                        className="h-full w-full object-cover"
-                      />
-                    }
-                    description={`Discover our ${category} collection.`}
-                    href={`/${category}`}
-                    cta={`Shop ${category}`}
-                  />
-                </BentoGrid>
-              </BackgroundGradient>
-            )}
-          </div>
-        ))
-      }
+      <div key={category} className="mb-8 w-full grid grid-cols-2 grid-rows-2">
+        {
+          category.map((category: string) => (
+            <BackgroundGradient key={category} className="mb-8">
+              <BentoGrid>
+                <BentoCard
+                  name={category.charAt(0).toUpperCase() + category.slice(1)}
+                  background={
+                    <img
+                      src={`https://ik.imagekit.io/lrigu76hy/tailark/${category}.jpg?updatedAt=1745733473768`}
+                      alt={category}
+                      className="h-full w-full object-cover"
+                    />
+                  }
+                  description={`Discover our ${category} collection.`}
+                  href={`/${category}`}
+                  cta={`Shop ${category}`}
+                />
+              </BentoGrid>
+            </BackgroundGradient>
+          ))
+        }
+      </div>
     </div>
   )
 }
